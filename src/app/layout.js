471787f8ca7d0fb1983eb37fd33a7c04b6f901aa
Tamzid  Ullah <tamzidullah@Tamzids-MacBook-Pro.local>
2024-10-06 22:45:13 -0400

    v1

--- src/app/layout.js
+++ src/app/layout.js
@@ -38,6 +38,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/png" href="/favicon-48x48.png" sizes="48x48" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="Stock Analysis Pro" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
