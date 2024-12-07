34f072e0a6edc5ff5995316c90fc621f533d0f74
Tamzid Ullah <tamzid257@gmail.com>
2024-12-07 11:26:10 -0500

    update

--- src/app/components/website/AIChat.js
+++ src/app/components/website/AIChat.js
@@ -185,7 +185,6 @@ const AIChat = ({ stockData }) => {
     mb: 1,
     whiteSpace: 'pre-wrap',
     wordBreak: 'break-word',
-    // Add the properties below to make the box scrollable
     maxHeight: '40vh',
     overflowY: 'auto'
   }}
@@ -194,8 +193,14 @@ const AIChat = ({ stockData }) => {
     children={message.content}
     remarkPlugins={[remarkMath]}
     rehypePlugins={[rehypeKatex]}
+    components={{
+      p: ({ node, ...props }) => (
+        <p style={{ margin: 0, padding: 0, lineHeight: '1.4' }} {...props} />
+      ),
+    }}
   />
 </Box>
+
 
 
               {message.role === 'assistant' && !feedbackGiven[index] && !isAssistantTyping && (
